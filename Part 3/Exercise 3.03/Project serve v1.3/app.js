5fbcd6563ba9a0138f6a45597cf07565464ebd78
DanielForsell <daniel.forsell@student.oulu.fi>
2025-01-06 16:30:50 +0200

    Pipeline test

---
+++ Part 3/Exercise 3.03/Project serve v1.3/app.js
@@ -0,0 +1,28 @@
+const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const imageRouter = require('./controllers/image');
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+// logger.info('connecting to', config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
+app.use(middleware.userExtractor)
+
+app.use('/api/image', imageRouter)
+
+app.get('/', (req, res) => {
+  res.status(200)
+})
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
